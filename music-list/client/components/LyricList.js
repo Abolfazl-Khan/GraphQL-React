2d1556a2937fade161a3112629c9e0a165f2f387
Abolfazl-Khan <abolfazlkhan.m@gmail.com>
2022-12-01 14:51:23 +0330

    updated LyricList to add like mutation in music-list

--- music-list/client/components/LyricList.js
+++ music-list/client/components/LyricList.js
@@ -1,13 +1,31 @@
 import React from 'react';
+import { useMutation, gql } from '@apollo/client';
+
+const LIKE_LYRIC = gql`
+  mutation LikeLyric($id: ID!) {
+    likeLyric(id: $id) {
+      id
+      likes
+    }
+  }
+`;
 
 function LyricList(props) {
+  const [likeLyric] = useMutation(LIKE_LYRIC);
+
+  const onLike = (id) => {
+    likeLyric({ variables: { id } });
+  };
+
   const renderLyrics = () =>
     props.lyrics?.map(({ id, content, likes }) => {
       return (
         <li key={id} className='collection-item'>
           {content}
           <div className='vote-box'>
-            <i className='material-icons'>thumb_up</i>
+            <i className='material-icons' onClick={() => onLike(id)}>
+              thumb_up
+            </i>
             {likes}
           </div>
         </li>
